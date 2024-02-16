75273465df67b2f39a36405657effb4a6fba9b92
sreynuch17 <sreynuch.thoeun@student.passerellesnumeriques.org>
2024-02-16 10:07:38 +0700

    [feat] phone number input of the form FP-21

--- controllers/signup/signup.controller.php
+++ controllers/signup/signup.controller.php
@@ -2,7 +2,13 @@
 <?php
 require "views/signup/signup.view.php";
 
-// ----get data from user input------
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    // TO DO-----------
+// ----get data from user input------   
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+     // TO DO-----------
+    if (isset($_POST['phone']) && !empty($_POST['phone'])){
+        $phone = $_POST['phone'];
+        echo "Your number phone is: " .$phone;
+    }else {
+        echo "Please enter your number phone!";
+    }
 }
