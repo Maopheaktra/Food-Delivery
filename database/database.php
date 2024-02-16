adfc11f3efd8e4a671fee3182d607171ce728955
CHANTHOU.VOEUN <CHANTHOU.VOEUN@pnc.lan>
2024-02-16 07:59:42 +0700

    [feat] Connect database to the project #12

--- database/database.php
+++ database/database.php
@@ -1,9 +1,18 @@
 <?php
 
-// $hostname = "localhost";
-// $database = "demo_db";
-// $username = "root";
-// $password = "";
+$hostname = "localhost";
+$database = "panda_db";
+$username = "root";
+$password = "";
 
-// $dsn = "mysql:host=$hostname;dbname=$database;charset=utf8mb4";
-// $connection = new PDO($dsn, $username, $password);
+$dsn = "mysql:host=$hostname;dbname=$database;charset=utf8mb4";
+$connection = new PDO($dsn, $username, $password);
+
+// test the result--------
+$statement = $connection->prepare("select * from users");
+$statement->execute();
+$value = $statement->fetch();
+print_r ($value[2]);
+
+echo "I connected the database";
+
