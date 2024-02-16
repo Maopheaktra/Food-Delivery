d8fd37d2820fbac5202e1dc401a95fbb80163e49
PHEAKTRA.MAO <PHEAKTRA.MAO@PNC.LAN>
2024-02-16 10:04:04 +0700

    [feat] coding on the gender input of the form signup #FP-20

--- controllers/signup/show.controller.php
+++ controllers/signup/show.controller.php
@@ -4,8 +4,10 @@
 // ----get data from user input------
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     // TO DO-----------
-    if (isset($_POST['usernames']) && !empty($_POST['usernames'])){
+    if (isset($_POST['usernames']) && !empty($_POST['usernames'] && !empty($_POST['gender']))){
         $usernames = $_POST['usernames'];
+        $gender = $_POST['gender'];
+        echo "Your gender is: " .$gender; 
         echo "Your username is: " .$usernames;
     }else {
         echo "Please enter your username!";
