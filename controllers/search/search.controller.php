156626bd9f180755dffc3c93d7bedda09ccaa66f
PHEAKTRA.MAO <PHEAKTRA.MAO@PNC.LAN>
2024-02-19 11:21:26 +0700

    [feat] search food #FP-77

--- controllers/search/search.controller.php
+++ controllers/search/search.controller.php
@@ -1,3 +1,3 @@
 <?php
-
 require "views/search/search.view.php";
+
--- database/database.php
+++ database/database.php
@@ -4,13 +4,6 @@ $hostname = "localhost";
 $database = "panda_db";
 $username = "root";
 $password = "";
-<<<<<<< HEAD
 
 $dsn = "mysql:host=$hostname;dbname=$database;charset=utf8mb4";
 $connection = new PDO($dsn, $username, $password);
-
-=======
-
-$dsn = "mysql:host=$hostname;dbname=$database;charset=utf8mb4";
-$connection = new PDO($dsn, $username, $password);
->>>>>>> signin
