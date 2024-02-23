0cfe1a1906ba1c0e279394889287184111898cd1
sreynuch17 <sreynuch.thoeun@student.passerellesnumeriques.org>
2024-02-23 11:05:00 +0700

    [feat] update #FP-65

--- controllers/signin/signin.controller.php
+++ controllers/signin/signin.controller.php
@@ -2,33 +2,3 @@
 
 require "views/signin/signin.view.php";
 require "models/user.model.php";
-
-// function login($email, $password): void
-// {
-//     global $connection;
-//     try {
-//         $user_info = [
-//             'email' => $email,
-//             'pass' => $password
-//         ];
-
-//         $sanitizedData = sanitizeValues($user_info);
-
-//         if ($sanitizedData !== null) {
-//             $stmt = $connection->prepare('SELECT * FROM users WHERE email = :email,password = :password');
-//             $stmt->bindParam(':email', $sanitizedData['email']);
-//             $stmt->bindParam(':password', $sanitizedData['password']);
-//             $stmt->execute();
-//             $user_data = $stmt->fetch();
-
-//             if ($user_data && password_verify($password, $user_data['password'])) {
-//                 session_start();
-//                 $_SESSION['user'] = $user_data;
-//                 header('Location:/profile ');
-//             }
-//         }
-//     } catch (Exception $e) {
-//         error_log($e->getMessage());
-//         echo "An unexpected error occurred.";
-//     }
-// }
