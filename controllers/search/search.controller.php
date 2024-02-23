ddb6428028285ff92e90dd198e0d11066a239306
PHEAKTRA.MAO <PHEAKTRA.MAO@PNC.LAN>
2024-02-23 08:34:16 +0700

    [ref] search food for user #FP-77

--- controllers/search/search.controller.php
+++ controllers/search/search.controller.php
@@ -1,19 +1,3 @@
 <?php
-require "database/database.php";
+
 require "views/search/search.view.php";
-
-
-$searchError = '';
-$food_name = '';
-//----------------TO DO SEARCH FOOD--------------------------
-if (isset($_POST['search'])) {
-    if (!empty($_POST['search'])) {
-        # code...
-        $searchFood = $_POST['search'];
-        $statement = $connecton-> prepare("SELECT * FROM foods WHERE foodname LIKE '%$searchFood%'");
-        $statement-> execute();
-        $food_name = $statement->fetch_all();
-    }else{
-        $searchError  = "We cannot find this food";
-    }
-}
