5c20812ed2d2d4aee3e3bee70d860efab11a8e11
VoeunChanthou <chanthou.voeun@student.passerellesnumeriques.org>
2024-03-17 22:28:41 +0700

    update user panel

--- controllers/delivery/delivery.controller.php
+++ controllers/delivery/delivery.controller.php
@@ -1,18 +1,6 @@
-
 <?php
-if (isset($_POST['submit_address'])){
-    $address = urlencode($_POST['address']);
-    $address = str_replace(" ", "+", $address);
-    ?>
-    <iframe width="100%" height="500" src="https://maps.google.com/maps?q=<?php echo $address; ?>&output=embed></iframe>
-    <?php
-}
-if (isset($_POST['submit_coordinates'])) {
-    $latitude = $_POST['latitude'];
-    $longitude = $_POST['longitude'];
-    ?>
-    <iframe width="100%" height="500" src="https://maps.google.com/maps?q=<?php echo $latitude; ?>,<?php echo $longitude; ?>&output=embe"></iframe>
-    <?php
-}
-?>
+
+require "views/delevery/delevery.view.php";
+
+
 
