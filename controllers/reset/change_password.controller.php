546e0591ebcca828a263219d36c8f1b6ee896502
sreynuch17 <sreynuch.thoeun@student.passerellesnumeriques.org>
2024-03-18 08:48:21 +0700

    [feat] code change password #FP-148

--- controllers/reset/change_password.controller.php
+++ controllers/reset/change_password.controller.php
@@ -1,1 +1,23 @@
+<?php
 
+require "../../database/database.php";
+require "../../models/password_change_model.php";
+
+
+$email = $_POST['email'];
+$currentPassword = $_POST['current_password'];
+$newPassword = $_POST['new_password'];
+$confirmPassword = $_POST['confirm_password'];
+
+
+if($newPassword == $confirmPassword){
+
+    if (validateCurrentPassword($email, $currentPassword)){
+        $newPwd = password_hash($_POST['new_password'], PASSWORD_BCRYPT);
+        updatePassword($email, $newPassword);
+    }else{
+        echo "Insecces";
+    }
+}else{
+    echo 'Insecces';
+}
